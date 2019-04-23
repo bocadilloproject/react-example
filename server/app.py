5a0566a2c5a60106a0ca20ef7a64f2a37d8c700c
Chad Smith <cs01@users.noreply.github.com>
2019-04-23 09:52:42 -0700

    render arbitrary templates based on url

--- server/app.py
+++ server/app.py
@@ -10,11 +10,16 @@ templates = Templates(app, directory=settings.TEMPLATES_DIR)
 
 @app.route("/")
 async def index(req, res):
-    res.html = await templates.render("index.html")
+    res.html = await templates.render(f"index.html")
+
+
+@app.route("/page/{name}")
+async def render_page(req, res, name):
+    res.html = await templates.render(f"{name}")
 
 
 @app.route("/api/message")
 async def api(req, res):
-    now = time.strftime("%I:%M:%S %p")
-    message = f"Hello from the Bocadillo server! The time is {now}."
+    current_time = time.strftime("%I:%M:%S %p")
+    message = f"Hello from the Bocadillo server! The time is {current_time}."
     res.json = {"message": message}
